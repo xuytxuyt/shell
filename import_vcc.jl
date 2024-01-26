e33c3219d5ac716243139852ed02ffe38cdfda19
WuJC <jcwu@hqu.edu.cn>
2024-01-26 22:22:52 +0800

    temp

--- import_vcc.jl
+++ import_vcc.jl
@@ -13,10 +13,13 @@ function import_vcc(filename::String)
     𝒂₁ = cs.𝒂₁
     𝒂₂ = cs.𝒂₂
     𝒂₃ = cs.𝒂₃
+    a₁₁ = cs.a₁₁
+    a₂₂ = cs.a₂₂
+    a₁₂ = cs.a₁₂
     integrationOrder = 10
     elements = Dict{String,Vector{ApproxOperator.AbstractElement}}()
     elements["Ω"] = getCurvedElements(nodes, entities["Ω"], integrationOrder,𝐽=𝐽)
-    elements["Γ"] = getCurvedElements(nodes, entities["Γ"], integrationOrder, normal = true,𝒂₁=𝒂₁,𝒂₂=𝒂₂,𝒂₃=𝒂₃)
+    elements["Γ"] = getCurvedElements(nodes, entities["Γ"], integrationOrder,𝒂₁=𝒂₁,𝒂₂=𝒂₂,𝒂₃=𝒂₃,a₁₁=a₁₁,a₂₂=a₂₂,a₁₂=a₁₂)
 
     return elements, nodes
 end
