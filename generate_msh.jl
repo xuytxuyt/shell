3bf3b6f6459333914d78d24f70076c202c9b2992
jcwu <jcwu@hqu.edu.cn>
2024-02-06 00:29:14 +0800

    temp

--- generate_msh.jl
+++ generate_msh.jl
@@ -5,4 +5,4 @@ n = 1
 lc = 0.1
 # BenchmarkExample.ScordelisLoRoof.generateMsh("./msh/scordelislo_"*string(n)*".msh", transfinite = n)
 # BenchmarkExample.SphericalShell.generateMsh("./msh/sphericalshell_"*string(n)*".msh", transfinite = n)
-BenchmarkExample.PatchTestThinShell.generateMsh("./msh/patchtest.msh",lc = 0.5)
+BenchmarkExample.PatchTestThinShell.generateMsh("./msh/patchtest.msh",lc = lc)
